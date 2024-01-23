5d0bd4d44eea42386eff6566cffcf6e53eb0b44b
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-01-23 15:33:07 +0300

    task1. method printTwoNumbersBlok

---
+++ src/task_1/Main.java
@@ -0,0 +1,11 @@
+package task_1;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String docNumber = "2255-abC-4499-tUt-1o1o";
+
+        DocumentNumberFormat.printTwoNumbersBlock(docNumber);
+
+    }
+}
