e155a8b1991975f4cae975b5064c1261452e3772
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-01-24 13:52:18 +0300

    task2. method shortestAndLongestWord

--- src/task_2/StrLength.java
+++ src/task_2/StrLength.java
@@ -1,2 +1,35 @@
-package task_2;public class StrLenght {
+package task_2;
+
+//    2. Дана строка произвольной длины с произвольными словами.
+//    Найти самое короткое слово в строке и вывести его на экран.
+//    Найти самое длинное слово в строке и вывести его на экран.
+//    Если таких слов несколько, то вывести последнее из них.
+
+public class StrLength {
+    public static void shortestAndLongestWord(String str) {
+        String[] words = str.split("[\\s\\p{Punct}—]+");
+
+        String shortestWord = words[0];
+        String longestWord = words[0];
+
+        for (String word : words) {
+            if (word.length() <= shortestWord.length()) {
+                shortestWord = word;
+            }
+            if (word.length() >= longestWord.length()) {
+                longestWord = word;
+            }
+        }
+
+        System.out.println("Дана строка из слов: \n" + str);
+        System.out.println("Самое короткое слово - " + shortestWord);
+        System.out.println("Самое длинной слово - " + longestWord);
+    }
+
+
+    public static void main(String[] args) {
+        String message = "Я хочу зарабатывать пять тысяч долларов, я - фантазёр!";
+
+        shortestAndLongestWord(message);
+    }
 }
