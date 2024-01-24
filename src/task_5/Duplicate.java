b7f05ee7e2618cb1327a505707dfa4245b5f9e8c
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-01-24 18:27:44 +0300

    task5. filter for non-letter characters

--- src/task_5/Duplicate.java
+++ src/task_5/Duplicate.java
@@ -5,16 +5,20 @@ package task_5;
 
 public class Duplicate {
     public static void doubleLetters(String str) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            result.append(str.charAt(i)).append(str.charAt(i));
-        }
-        System.out.println(result);
 
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i))) {
+                result.append(str.charAt(i)).append(str.charAt(i));
+            } else result.append(str.charAt(i));
+        }
+
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
-        String stroka = "Hello";
+        String stroka = "Hello, mandarin 123!";
         doubleLetters(stroka);
 
     }
